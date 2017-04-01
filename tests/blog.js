1cc1e3fa2784daee8e1bab2d776801925cf6766c
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-04-01 11:30:58 +0200

    Add basic testcafe tests
    Fixes #19

---
+++ tests/blog.js
@@ -0,0 +1,37 @@
+import { Selector } from 'testcafe';
+
+fixture `Blog`
+  .page `http://localhost:1313/blog/`;
+
+test(`the page contains a robots tag with noindex value`, async t => {
+  const robots = await Selector(`[name="robots"]`).getAttribute(`content`);
+
+  await t.expect(robots).contains(`noindex`);
+});
+
+test(`less than 11 articles are shown`, async t => {
+  const articleCount = await Selector(`.c-article-list__item`).count;
+
+  await t.expect(articleCount < 11).ok();
+});
+
+test(`blog article contains important header data`, async t => {
+  await t.click('.c-article-list__cta');
+
+  const title = await Selector(`title`).innerText;
+  const description = await Selector(`[name="description"]`).getAttribute(`content`);
+  const author = await Selector(`[name="author"]`).exists;
+  const themeColor = await Selector(`[name="theme-color"]`).exists;
+  const robots = await Selector(`[name="robots"]`).exists;
+  const canonical = await Selector(`[rel="canonical"]`).exists;
+  const manifest = await Selector(`[rel="manifest"]`).exists;
+
+  await t
+    .expect(title.length > 20).ok()
+    .expect(description.length > 70).ok()
+    .expect(author).ok()
+    .expect(themeColor).ok()
+    .expect(robots).notOk()
+    .expect(canonical).ok()
+    .expect(manifest).ok();
+});
---
+++ tests/home.js
@@ -0,0 +1,29 @@
+import { Selector } from 'testcafe';
+
+fixture `Home`
+  .page `http://localhost:1313/`;
+
+test(`the page contains all important header data`, async t => {
+  const title = await Selector(`title`).innerText;
+  const description = await Selector(`[name="description"]`).getAttribute(`content`);
+  const author = await Selector(`[name="author"]`).exists;
+  const themeColor = await Selector(`[name="theme-color"]`).exists;
+  const robots = await Selector(`[name="robots"]`).exists;
+  const canonical = await Selector(`[rel="canonical"]`).exists;
+  const manifest = await Selector(`[rel="manifest"]`).exists;
+
+  await t
+    .expect(title.length > 20).ok()
+    .expect(description.length > 70).ok()
+    .expect(author).ok()
+    .expect(themeColor).ok()
+    .expect(robots).notOk()
+    .expect(canonical).ok()
+    .expect(manifest).ok();
+});
+
+test(`the latest 3 articles are shown`, async t => {
+  const articleCount = await Selector(`.c-article-list__item`).count;
+
+  await t.expect(articleCount === 3).ok();
+});
