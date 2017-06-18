a3ae9961cebbc7a363613bcf0a81071a49ad1008
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-06-18 18:11:45 +0200

    Fix linting errors

--- gulpfile.js
+++ gulpfile.js
@@ -18,10 +18,10 @@ gulp.task(`watch`, () => {
 gulp.task(`styles`, [`clean:styles`], () =>
   gulp.src(`themes/mao/src/scss/**/*.scss`)
     .pipe(sourcemaps.init())
-      .pipe(sass({
-        importer: nodeSassMagicImporter(),
-      }).on(`error`, sass.logError))
-      .pipe(autoprefixer())
+    .pipe(sass({
+      importer: nodeSassMagicImporter(),
+    }).on(`error`, sass.logError))
+    .pipe(autoprefixer())
     .pipe(sourcemaps.write({ sourceRoot: `/scss` }))
     .pipe(gulp.dest(stylesDestDirectory))
 );
