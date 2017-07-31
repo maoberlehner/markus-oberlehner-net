69e0480c10638deed99e81a48cceeeafcce45b7b
Markus Oberlehner <markus.oberlehner@karriere.at>
2017-07-31 16:13:16 +0200

    Remove repititions of paths in the gulpfile (DRY!)

--- gulpfile.js
+++ gulpfile.js
@@ -3,37 +3,44 @@ const CleanCSS = require(`clean-css`);
 const gulp = require(`gulp`);
 const htmlmin = require(`gulp-htmlmin`);
 const inline = require(`gulp-inline-source`);
-const uncss = require(`uncss`);
 const nodeSassMagicImporter = require(`node-sass-magic-importer`);
+const path = require(`path`);
 const rimraf = require(`rimraf`);
 const sass = require(`gulp-sass`);
 const sourcemaps = require(`gulp-sourcemaps`);
+const uncss = require(`uncss`);
 
 const stylesDestDirectory = `static/dist/css`;
+const themeSrcDirectory = `themes/mao/src`;
+const scssRoot = `scss`;
+const scssPath = path.join(themeSrcDirectory, scssRoot, `**/*.scss`);
+
+const publicDirectory = `public`;
+const htmlPath = path.join(publicDirectory, `**/*.html`);
 
 gulp.task(`watch`, () => {
-  gulp.watch(`themes/mao/src/scss/**/*.scss`, [`styles`]);
+  gulp.watch(scssPath, [`styles`]);
 });
 
 gulp.task(`styles`, [`clean:styles`], () =>
-  gulp.src(`themes/mao/src/scss/**/*.scss`)
+  gulp.src(scssPath)
     .pipe(sourcemaps.init())
     .pipe(sass({
       importer: nodeSassMagicImporter(),
     }).on(`error`, sass.logError))
     .pipe(autoprefixer())
-    .pipe(sourcemaps.write({ sourceRoot: `/scss` }))
+    .pipe(sourcemaps.write({ sourceRoot: path.join(`/`, scssRoot) }))
     .pipe(gulp.dest(stylesDestDirectory))
 );
 
 gulp.task(`minify:markup`, () =>
-  gulp.src(`public/**/*.html`)
+  gulp.src(htmlPath)
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(inline({
-      rootpath: `public/`,
+      rootpath: path.join(publicDirectory, `/`),
       handlers: (source, context, next) => {
         if (source.type === `css` && source.fileContent && !source.content) {
-          uncss(context.html, { htmlroot: `public` }, (error, css) => {
+          uncss(context.html, { htmlroot: publicDirectory }, (error, css) => {
             if (error) throw error;
             // eslint-disable-next-line no-param-reassign
             source.content = `<style>${new CleanCSS({ level: 2 }).minify(css).styles}</style>`;
@@ -44,7 +51,7 @@ gulp.task(`minify:markup`, () =>
         }
       },
     }))
-    .pipe(gulp.dest(`public`))
+    .pipe(gulp.dest(publicDirectory))
 );
 
 gulp.task(`clean:styles`, () => rimraf.sync(stylesDestDirectory));
