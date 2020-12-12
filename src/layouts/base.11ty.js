a5dd84c9f3a2b3757224f78c3f36fd1b78d11b53
Markus Oberlehner <markus.oberlehner@gmail.com>
2020-12-12 12:11:00 +0100

    Prefer class syntax everywhere

--- src/layouts/base.11ty.js
+++ src/layouts/base.11ty.js
@@ -1,21 +1,27 @@
-module.exports = ({
-  content,
-  description,
-  meta,
-  title,
-}) => `<!doctype html>
-<html lang="${meta.lang}">
-  <head>
-    <meta charset="utf-8">
-    <title>${title || meta.title}</title>
-    <meta name="description" content="${description || meta.description}">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-  </head>
-  <body>
-    <div role="document">
-      <main id="main" tabindex="-1">
-        ${content}
-      </main>
-    </div>
-  </body>
-</html>`;
+module.exports = class LayoutsBase {
+  render({
+    content,
+    description,
+    meta,
+    title,
+  }) {
+    return `
+      <!doctype html>
+      <html lang="${meta.lang}">
+        <head>
+          <meta charset="utf-8">
+          <title>${title || meta.title}</title>
+          <meta name="description" content="${description || meta.description}">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+        </head>
+        <body>
+          <div role="document">
+            <main id="main" tabindex="-1">
+              ${content}
+            </main>
+          </div>
+        </body>
+      </html>
+    `;
+  }
+};
